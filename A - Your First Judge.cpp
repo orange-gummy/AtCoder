50f21c6afe0fa03dfaa1db5eb71e5f33bbb8c2c5
orange-gummy <tomo838.tomot@gmail.com>
2021-08-21 23:12:09 +0900

    fin

---
+++ A - Your First Judge.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	string s;
+	cin >> s;
+	if (s == "Hello,World!") cout << "AC" << endl;
+	else cout << "WA" << endl;
+	return 0;
+}
