c4d462eed4045c1f0581337cb02768a32c36adfd
orange-gummy <tomo838.tomot@gmail.com>
2021-09-24 21:52:26 +0900

    fin

--- JOI/JOI2020 2/B.cpp
+++ JOI/JOI2020 2/B.cpp
@@ -1,11 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
+using Graph = vector<vector<int>>;
 typedef long long ll;
 #define REP(i, a, b) for(int i = (a);i < (b);++i)
 #define REPS(i, a, b) for(int i = (b)-1;i >= (a);--i)
 #define rep(i, n) REP(i, 0, n)
 #define reps(i, n) REPS(i, 0, n)
 #define fora(i,I) for(const auto& i:I)
+#define max3(x, y, z) max(x, max(y, z))
+#define min3(x, y, z) min(x, min(y, z))
 //x:コンテナ
 #define ALL(x) x.begin(),x.end() 
 #define SIZE(x) ll(x.size())
@@ -19,11 +22,9 @@ ll myceil(ll a,ll b){return (a+(b-1))/b;}
 ll myfloor(ll a,ll b){return a/b;}
 #define endl '\n'
 #define int long long int
-signed main(){
+
+int main() {
 	cin.tie(0);cout.tie(0);
 	ios_base::sync_with_stdio(false);
-	//code start
 	
-	//code end
-	return 0;
 }
