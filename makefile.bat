e208428ba55891486529e03f1c484241d6fbdd01
orange-gummy <tomo838.tomot@gmail.com>
2021-10-02 11:31:37 +0900

    bat

---
+++ makefile.bat
@@ -0,0 +1,14 @@
+cd C:\Users\tomo_\Desktop\AtCoder
+md %1
+cd %1
+type nul > A.cpp
+type nul > B.cpp
+type nul > C.cpp
+
+
+type nul > D.cpp
+type nul > E.cpp
+type nul > F.cpp
+type nul > G.cpp
+type nul > H.cpp
+code A.cpp
