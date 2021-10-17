ab2bb863468eb26227e5446095ecfae9e35543e4
orange-gummy <tomo838.tomot@gmail.com>
2021-10-17 17:21:14 +0900

    fin

---
+++ ARC128/A.cpp
@@ -0,0 +1,41 @@
+/**
+*    author:  Dooloper
+*    created: 16.10.2021 21:00:49
+**/
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define REP(i, a, b) for(int i = (a);i < (b);++i)
+#define REPS(i, a, b) for(int i = (b)-1;i >= (a);--i)
+#define rep(i, n) REP(i, 0, n)
+#define reps(i, n) REPS(i, 0, n)
+#define fora(i,I) for(const auto& i:I)
+#define max3(x, y, z) max(x, max(y, z))
+#define min3(x, y, z) min(x, min(y, z))
+#define ALL(x) x.begin(),x.end() 
+#define SIZE(x) ll(x.size())
+#define F first
+#define S second
+#define coutALL(x) for(auto i=x.begin();i!=--x.end();i++)cout<<*i<<' ';cout<<*--x.end()<<endl;
+ll myceil(ll a,ll b){return (a+(b-1))/b;}
+ll myfloor(ll a,ll b){return a/b;}
+#define endl '\n'
+
+int main() {
+    cin.tie(0);cout.tie(0);
+    ios_base::sync_with_stdio(false);
+    //code start
+    int n;cin >> n;
+    vector<int> A(n);
+    vector<int> ans(n,0);
+    rep(i,n)cin>>A[i];
+    rep(i,n-1){
+        if(A[i]>A[i+1]){
+            ans[i]^=1;
+            ans[i+1]^=1;
+        }
+    }
+    rep(i,n)cout<<ans[i]<<' ';
+    //code end
+   return 0;
+}
