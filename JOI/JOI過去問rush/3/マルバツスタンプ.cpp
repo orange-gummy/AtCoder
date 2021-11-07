70f5ca168dd01ba7c5f1e8b03b0382d687bf5df0
orange-gummy <tomo838.tomot@gmail.com>
2021-11-07 19:32:26 +0900

    fin

---
+++ JOI/JOI過去問rush/3/マルバツスタンプ.cpp
@@ -0,0 +1,46 @@
+/**
+*    author:  orangegummy
+*    created: 07.11.2021 19:28:15
+**/
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define REP(i, a, b) for(int i = (a);i < (b);++i)
+#define REPS(i, a, b) for(int i = (b)-1;i >= (a);--i)
+#define rep(i, n) REP(i, 0, n)
+#define reps(i, n) REPS(i, 0, n)
+#define fora(i,I) for(const auto& i:I)
+#define max3(x, y, z) max(x, max(y, z))
+#define min3(x, y, z) min(x, min(y, z))
+#define all(x) x.begin(),x.end() 
+#define size(x) ll(x.size())
+#define F first
+#define S second
+#define coutall(x) for(auto i=x.begin();i!=--x.end();i++)cout<<*i<<endl;cout<<*--x.end()<<endl;
+#define coutall2(x) rep(j,size(x)){for(auto i=x[j].begin();i!=--x[j].end();i++)cout<<*i<<" ";cout<<*--x[j].end()<<endl;};
+#define vecmax(x) max_element(all(x));
+#define vecmin(x) min_element(all(x));
+#define yn if(ans){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;};
+#define endl '\n'
+
+int main() {
+  cin.tie(0);cout.tie(0);
+  ios_base::sync_with_stdio(false);
+  //code start
+  int n;cin>>n;
+  string s;cin>>s;
+  int ans=0;
+  vector<int> A(n,0);
+  rep(i,n-1){
+    if(A[i]==1)continue;
+    if(s[i]=='X'&&s[i+1]=='O'){
+      ++ans;A[i+1]=1;
+    }
+    if(s[i]=='O'&&s[i+1]=='X'){
+      ++ans;A[i+1]=1;
+    }
+  }
+  cout<<ans<<endl;
+  //code end
+  return 0;
+}
