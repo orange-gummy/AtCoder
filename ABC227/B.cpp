ccc068915831a2860d4e2ae58d8abf7bb875b600
orange-gummy <tomo838.tomot@gmail.com>
2021-11-19 00:20:57 +0900

    fin

---
+++ ABC227/B.cpp
@@ -0,0 +1,45 @@
+/**
+*    author:  orangegummy
+*    created: 13.11.2021 20:56:21
+**/
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define REP(i, a, b) for(int i = (a);i < (b);++i)
+#define REPS(i, a, b) for(int i = (b)-1;i >= (a);--i)
+#define rep(i, n) REP(i, 0, n)
+#define reps(i, n) REPS(i, 0, n)
+#define fora(i,I) for(const auto& i:I)
+#define max3(x, y, z) max(x, max(y, z))
+#define min3(x, y, z) min(x, min(y, z))
+#define all(x) x.begin(),x.end() 
+#define size(x) ll(x.size())
+#define F first
+#define S second
+#define coutall(x) for(auto i=x.begin();i!=--x.end();i++)cout<<*i<<endl;cout<<*--x.end()<<endl;
+#define coutall2(x) rep(j,size(x)){for(auto i=x[j].begin();i!=--x[j].end();i++)cout<<*i<<" ";cout<<*--x[j].end()<<endl;};
+#define vecmax(x) max_element(all(x))
+#define vecmin(x) min_element(all(x))
+#define yn if(ans){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;};
+#define endl '\n'
+
+int main() {
+  cin.tie(0);cout.tie(0);
+  ios_base::sync_with_stdio(false);
+  //code start
+  int n;cin>>n;
+  set<int> A;
+  REP(i,1,1000){
+    REP(j,1,1000){
+      A.insert(4*i*j+3*i+3*j);
+    }
+  }
+  int ans=0;
+  rep(i,n){
+    int a;cin>>a;
+    if(find(all(A),a)==A.end())++ans;
+  }
+  cout<<ans<<endl;
+  //code end
+  return 0;
+}
---
+++ ABC227/C.cpp
@@ -0,0 +1,40 @@
+/**
+*    author:  orangegummy
+*    created: 13.11.2021 20:56:24
+**/
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define REP(i, a, b) for(int i = (a);i < (b);++i)
+#define REPS(i, a, b) for(int i = (b)-1;i >= (a);--i)
+#define rep(i, n) REP(i, 0, n)
+#define reps(i, n) REPS(i, 0, n)
+#define fora(i,I) for(const auto& i:I)
+#define max3(x, y, z) max(x, max(y, z))
+#define min3(x, y, z) min(x, min(y, z))
+#define all(x) x.begin(),x.end() 
+#define size(x) ll(x.size())
+#define F first
+#define S second
+#define coutall(x) for(auto i=x.begin();i!=--x.end();i++)cout<<*i<<endl;cout<<*--x.end()<<endl;
+#define coutall2(x) rep(j,size(x)){for(auto i=x[j].begin();i!=--x[j].end();i++)cout<<*i<<" ";cout<<*--x[j].end()<<endl;};
+#define vecmax(x) max_element(all(x))
+#define vecmin(x) min_element(all(x))
+#define yn if(ans){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;};
+#define endl '\n'
+
+int main() {
+  cin.tie(0);cout.tie(0);
+  ios_base::sync_with_stdio(false);
+  //code start
+  int n;cin>>n;
+  set<tuple<ll,ll,ll>> A;
+  for(int i=1;i<pow(n,1/3);++i)for(int j=i;j<pow(n/i,1/2);++j){
+    for(int k=1;i<=n/(i*j);++k){
+      if(k>=i&&k>=j)A.insert(make_tuple(i,j,k));
+    }
+  }
+  cout<<size(A)<<endl;
+  //code end
+  return 0;
+}
