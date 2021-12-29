b16f810d45faf6bf705f64f261bc16bd35a8a867
orange-gummy <tomo838.tomot@gmail.com>
2021-12-29 16:14:11 +0900

    fin

--- makefile.bat
+++ makefile.bat
@@ -1,12 +1,5 @@
 cd C:\Users\tomo_\Desktop\AtCoder\%1
 md %1%2
 cd %1%2
-type nul > A.cpp
-type nul > B.cpp
-type nul > C.cpp
-type nul > D.cpp
-type nul > E.cpp
-type nul > F.cpp
-type nul > G.cpp
-type nul > H.cpp
-code A.cpp
+type nul > %3.cpp
+code %3.cpp
