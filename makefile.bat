c3812635a97e6840e86055654b247e5683eb5382
orange-gummy <tomo838.tomot@gmail.com>
2022-01-04 14:39:41 +0900

    fix

--- makefile.bat
+++ makefile.bat
@@ -1,5 +1,19 @@
+@echo off
 cd C:\Users\tomo_\Desktop\AtCoder\%1
-md %1%2
-cd %1%2
-type nul > %3.cpp
+set FOLDER1=C:\Users\tomo_\Desktop\AtCoder\%1\%1%2
+set FOLDER2=C:\Users\tomo_\Desktop\AtCoder\%1\%1%2\%3.cpp
+if exist "%FOLDER1%" (
+  cd %1%2
+	if not exist "%FOLDER2%" (
+    type nul > %3.cpp
+    type temp > %3.cpp
+  )
+) else (
+  md %1%2
+  cd %1%2
+	if not exist "%FOLDER2%" (
+    type nul > %3.cpp
+    type temp > %3.cpp
+  )
+)
 code %3.cpp
